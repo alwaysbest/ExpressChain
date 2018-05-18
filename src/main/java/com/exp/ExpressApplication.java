60455eaca83de3b007599b24a6c2d293893c9045
lyin14 <lyin14@software.nju.edu.cn>
2018-05-18 18:46:03 +0800

    package

--- src/main/java/com/exp/ExpressApplication.java
+++ src/main/java/com/exp/ExpressApplication.java
@@ -1,22 +1,21 @@
 package com.exp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-//@MapperScan("com.ecm.dao")
+@MapperScan("com.ecm.bean")
 public class ExpressApplication extends SpringBootServletInitializer {
 
 	
 
-//	public static void main(String[] args) {
-//		SpringApplication.run(EcNetApplication.class, args);
-//	}
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressApplication.class, args);
+	}
 
 
 
