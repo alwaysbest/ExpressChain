1dc86cb1ec70ebc0a43212d57a1f1861c4a16112
lyin14 <lyin14@software.nju.edu.cn>
2018-05-20 00:19:57 +0800

    注释

--- src/main/java/com/exp/dao/BusinessInfoDao.java
+++ src/main/java/com/exp/dao/BusinessInfoDao.java
@@ -3,7 +3,7 @@ package com.exp.dao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.exp.model.BusinessInfo;
-
+//快递点信息
 public interface BusinessInfoDao extends JpaRepository<BusinessInfo, Integer>{
 	public BusinessInfo findById(int id);
 }
